bb5f38d9803e1fc0e16b934a5667573af04ec184
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-06 11:11:56 +0800

    Update src/views/home

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="home">
+  <div id="home" v-if="$store.state.login">
+    <ChatRoom />
   </div>
 </template>
 
 <script>
+import ChatRoom from "./chatroom";
+
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+    }
+  },
+  components: {
+    ChatRoom
+  },
 }
 </script>
