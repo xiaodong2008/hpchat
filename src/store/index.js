d22e18f89d68ed32e7a7be24c5ee2924cf8785a1
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-06 11:11:31 +0800

    Update src/store

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,16 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: {
+    login: false,
+    ws: null,
+    user: {},
+    friends: []
   },
   mutations: {
+    login(state, userdata) {
+      state.login = true;
+      state.user = userdata;
+    }
   },
   actions: {
   },
