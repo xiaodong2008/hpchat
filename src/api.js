59e28fdcb78b119e7f8be2f12e96bfa3a29bcdec
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-06 11:03:59 +0800

    Update src/api.js

--- src/api.js
+++ src/api.js
@@ -1,36 +1,54 @@
 import {FastjsAjax} from "fastjs-next";
 import {message} from "ant-design-vue";
-import langSetup from "./lang"
 import cookie from "js-cookie";
 
 const lang = cookie.get("language");
-const error = langSetup("config").error.api
 
-const resolve = (path, method = "get", data, config) => {
-  return new Promise((resolve) => {
-    config.callback = res => {
-      resolve(res);
+const resolve = (path, method = "get", data, config = {}) => {
+  if (cookie.get("token")) {
+    if (!config.headers) {
+      config.headers = {};
     }
-    config.failed = ajax => {
-      const status = ajax.xml.status;
-      if (status !== 0) {
-        const result = ajax.xml.response;
-        const msg = result[`${lang}-message`]
-        message.error(`[${status}] ${error.short}: ${msg}`);
-        console.error(`[${status}] ${error.short}: ${msg}`);
-      } else
-        message.error(error.default);
-      throw new Error(ajax);
+    config.headers.Authorization = cookie.get("token");
+  }
+  console.log(config);
+  return new Promise((resolve, reject) => {
+      new FastjsAjax(
+        path,
+        data,
+        config
+      ).send(method)
+        .then(res => {
+          if (res.message) {
+            if (res.success)
+              message.success(res.message[`${lang}`]);
+            else
+              message.info(res.message[`${lang}`]);
+          }
+          resolve(res)
+        }).catch(
+        err => {
+          if (err instanceof Error) {
+            console.log("msg",err.message)
+            // 保留第一行
+            err = err.message.split("\n")[0];
+            err = err.replace(/\[.*] /, "");
+            message.error(err);
+          } else {
+            message.error(err.message[`${lang}`]);
+          }
+          reject(err)
+        }
+      )
     }
-    new FastjsAjax(
-      path,
-      data,
-      config
-    ).send(method);
-  })
+  )
 }
 
 
 export function isLogin() {
-  return resolve("/api/user/userdata")
+  return resolve("/api/user/userdata");
+}
+
+export function login(uname, password) {
+  return resolve("/api/user/login", "post", {uname, password});
 }
