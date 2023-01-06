241e5a4e3488dd8b0688e2a3a6a2aecbecae7d47
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-06 11:13:52 +0800

    Update src/views/home/chatroom/chatindex

---
+++ src/views/home/chatroom/chatindex/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="chatindex">
+    <div class="sidebar">
+      <div class="user" v-for="user in $store.state.friends">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="less" scoped>
+#chatindex {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  .sidebar {
+    width: 240px;
+    height: 100%;
+    background-color: #f0f0f0;
+    border-right: 1px solid #d9d9d9;
+    .user {
+      width: 100%;
+      height: 50px;
+      background-color: #fff;
+      border-bottom: 1px solid #d9d9d9;
+    }
+  }
+}
+</style>
