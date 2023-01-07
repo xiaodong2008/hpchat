1afcfda1a6598f3f8327f88e2fba1f8a0985544f
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-07 20:12:07 +0800

    Update src/views/register

---
+++ src/views/register/index.vue
@@ -0,0 +1,171 @@
+<template>
+  <div id="register">
+    <a-form
+        :model="formState"
+        name="normal_login"
+        class="register-form"
+        @finish="onFinish"
+        @finishFailed="onFinishFailed"
+        :rules="formRules"
+    >
+      <a-form-item v-if="success">
+        <a-alert
+            :message="lang.name.success.replace('{uid}', userid)"
+            type="success"
+            show-icon></a-alert>
+      </a-form-item>
+
+      <a-form-item
+          :label="lang.name.username"
+          name="email"
+      >
+        <a-input v-model:value="formState.email">
+          <template #prefix>
+            <UserOutlined class="site-form-item-icon"/>
+          </template>
+        </a-input>
+      </a-form-item>
+
+      <a-form-item
+          :label="lang.name.password"
+          name="password"
+      >
+        <a-input-password v-model:value="formState.password">
+          <template #prefix>
+            <LockOutlined class="site-form-item-icon"/>
+          </template>
+        </a-input-password>
+      </a-form-item>
+
+      <a-form-item
+          :label="lang.name.repeat"
+          name="repeat"
+      >
+        <a-input-password v-model:value="formState.repeat">
+          <template #prefix>
+            <LockOutlined class="site-form-item-icon"/>
+          </template>
+        </a-input-password>
+      </a-form-item>
+
+      <a-form-item class="bottom">
+        <a-button :disabled="disabled" type="primary" html-type="submit" class="register-form-button">
+          {{ lang.name.register }}
+        </a-button>
+        {{ lang.name.or }}
+        <router-link to="/login">
+          {{ lang.name.login }}
+        </router-link>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+import {UserOutlined, LockOutlined} from '@ant-design/icons-vue';
+import langSetup from "@/lang";
+import {message} from "ant-design-vue";
+import {register} from "@/api.js";
+
+const lang = langSetup("register");
+
+export default {
+  name: "index",
+  data() {
+    const email = (_rule, val) => {
+      if (val.length > 50) {
+        return Promise.reject(lang.rules.username.length);
+      }
+      // 格式验证
+      if (/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)$/g.test(val)) {
+        return Promise.resolve();
+      } else {
+        return Promise.reject(lang.rules.username.format);
+      }
+    }
+    const password = (_rule, val) => {
+      if (val.length < 5)
+        return Promise.reject(lang.rules.password.minlength);
+      if (val.length > 50)
+        return Promise.reject(lang.rules.password.maxlength);
+      // 格式验证
+      if (/^[A-Za-z0-9_-]+$/g.test(val))
+        return Promise.resolve();
+      return Promise.reject(lang.rules.password.format);
+    }
+    const repeat = (_rule, val) => {
+      console.log("repeat", val);
+      if (val !== this.formState.password)
+        return Promise.reject(lang.rules.password.repeat);
+      return Promise.resolve();
+    }
+
+    return {
+      success: false,
+      userid: null,
+      formState: {
+        email: "",
+        password: "",
+        remember: true
+      },
+      formRules: {
+        email: [
+          {required: true, message: lang.rules.username.required},
+          {validator: email, trigger: "change"}
+        ],
+        password: [
+          {required: true, message: lang.rules.password.required},
+          {validator: password, trigger: "change"}
+        ],
+        repeat: [
+          {required: true, message: lang.rules.password.required},
+          {validator: repeat, trigger: "change"}
+        ]
+      },
+      disabled: false,
+      lang
+    }
+  },
+  methods: {
+    onFinish() {
+      this.disabled = true;
+      register(this.formState.email, this.formState.password).then(res => {
+        this.success = true;
+        this.userid = res.userid;
+      }).catch((err) => {
+        if (!err.message) message.error(lang.message.error);
+        this.disabled = false;
+      });
+    },
+    onFinishFailed() {
+      this.disabled = false;
+    }
+  },
+  components: {
+    UserOutlined,
+    LockOutlined
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#register {
+  width: 380px;
+  height: 400px;
+  padding: 30px;
+  background: #fff;
+  border-radius: 6px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+
+  .bottom {
+    position: absolute;
+    bottom: 0;
+    right: 30px;
+  }
+}
+</style>
