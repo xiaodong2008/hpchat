694607faa2dd1989e12fc5dc7ce7e51aa87a106e
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-09 22:49:44 +0800

    Update node/database.js

--- node/database.js
+++ node/database.js
@@ -16,41 +16,46 @@ const connection = mysql.createConnection({
 module.exports = async res => {
   let response
   if (res) response = require('./response.js')(res);
-  const query = (sql, params, callback) => {
-    return new Promise(async (resolve) => {
+  const query = (sql, params, handle_error = false) => {
+    return new Promise(async (resolve, reject) => {
       connection.query(sql, params, (err, result) => {
         if (err) {
-          if (callback) callback(err);
           if (!fs.existsSync('./error_log.txt')) {
             fs.writeFileSync('./error_log.txt', '');
           }
           const file = fs.readFileSync('error_log.txt');
-          const path = res.req.url;
+          const path = res?.req?.url || 'unknown';
           const message = `[${new Date().toLocaleString()}] ${path} | ${sql} | ${err.message}\n`;
           fs.writeFileSync('error_log.txt', message + file);
-          if (res) response(500, {
+          if (handle_error) {
+            reject(result);
+          } else if (res) response(500, {
             "message": {
               "zh": "数据库异常，请查看日志",
             }
           })
         } else {
           resolve(result);
-          if (callback) callback(null, result);
         }
       })
     })
   }
+  const now = () => {
+    return Date.now() + new Date().getTimezoneOffset() * 60 * 1000;
+  }
   return {
     query,
+    // get now time (utc +0) in timestamp
+    now,
     user: {
-      isLogin(token = res.req.headers.authorization) {
+      isLogin(token = res?.req?.headers?.authorization) {
         return new Promise((resolve) => {
           if (!token) {
             resolve(false);
           } else {
             query(
               "select * from `userlogin` where `token` = ? and `expire_time` > ? limit 1",
-              [token, new Date().toISOString().replace('T', ' ').replace('Z', '')]
+              [token, now()]
             ).then(result => {
               if (result.length === 0) {
                 resolve(false);
@@ -61,11 +66,16 @@ module.exports = async res => {
           }
         })
       },
-      getData(userid) {
+      getData(userinfo) {
         return new Promise((resolve) => {
+          let sql
+          // if uid
+          if (/^\d+$/.test(userinfo)) sql = "select * from `userdata` where `userid` = ? limit 1";
+          // if email
+          else if (/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(userinfo)) sql = "select * from `userdata` where `email` = ? limit 1";
           query(
-            "select * from `userdata` where `userid` = ? limit 1",
-            [userid]
+            sql,
+            [userinfo]
           ).then(result => {
             if (result.length === 0) {
               resolve(false);
@@ -80,7 +90,7 @@ module.exports = async res => {
       isFriend(from, to) {
         return new Promise((resolve) => {
           query(
-            "select * from `friend` where (`from` = '?' or `from` = '?') and (`to` = '?' or `to` = '?') limit 1",
+            "select * from `friend` where (`from` = ? or `from` = ?) and (`to` = ? or `to` = ?) limit 1",
             [from, to, from, to]
           ).then(result => {
             resolve(result.length !== 0 && result[0].to_delete === '0' && result[0].from_delete === '0' && result[0].approved === '1');
@@ -88,15 +98,18 @@ module.exports = async res => {
         })
       },
       getFriends(userid) {
-        let log = "select * from `friend` where (`from` = '?' and `approve` = '1' and `from_delete` = '0') or (`to` = '?' and `approve` = '1' and `to_delete` = '0')"
-        log = log.replace(/\?/g, userid);
-        console.log(log);
+        const friends = [];
         return new Promise((resolve) => {
           query(
-            "select * from `friend` where (`from` = ? and `approve` = '1' and `from_delete` = '0') or (`to` = ? and `approve` = '1' and `to_delete` = '0')",
+            "select * from `friend` where `from` = ? or `to` = ?",
             [userid, userid]
           ).then(result => {
-            resolve(result);
+            for (let friend of result) {
+              if (friend.to_delete === 0 && friend.from_delete === 0 && friend.approve === 1) {
+                friends.push(friend);
+              }
+            }
+            resolve(friends);
           })
         })
       }
