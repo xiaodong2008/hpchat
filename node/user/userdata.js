482e4195fa863da2b04e10fb2d77cedf99e56755
DY_XiaoDong <xiaodong@indouyin.cn>
2023-01-09 22:54:46 +0800

    Update node/user/userdata.js

--- node/user/userdata.js
+++ node/user/userdata.js
@@ -7,54 +7,22 @@ async function userdata(req, res) {
     return response(405, `405 Method Not Allowed: ${req.method}`, false);
   }
 
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return response(200, {
-      "login": false,
-      "userid": null,
-      "nickname": null
-    })
-  } else {
-    mysql.query(
-      "select * from `userlogin` where `token` = ? and `expire_time` > ? limit 1",
-      [token, new Date().toISOString().replace('T', ' ').replace('Z', '')]
-    ).then(
-      result => {
-        if (result.length === 0) {
-          return response(200, {
-            "login": false,
-            "userid": null,
-            "nickname": null
-          })
-        }
-        result = result[0];
-        mysql.user.getData(result.userid).then(
-          result => {
-            if (result.length === 0) {
-              return response(200, {
-                "login": false,
-                "userid": null,
-                "nickname": null
-              })
-            }
-            // update expire time
-            let expire_time = new Date().getTime() + 1000 * 60 * 60 * 24;
-            expire_time = new Date(expire_time).toISOString().replace('T', ' ').replace('Z', '');
-            mysql.query(
-              "update `userlogin` set `expire_time` = ? where `token` = ?",
-              [expire_time, token]
-            ).then(() => {
-              return response(200, {
-                "login": true,
-                "userid": result.userid,
-                "nickname": result.nickname
-              })
-            })
-          }
+  mysql.user.isLogin().then(
+    async result => {
+      if (result) {
+        // update expire time
+        let expire_time = mysql.now() + 1000 * 60 * 60 * 24;
+        await mysql.query(
+          "update `userlogin` set `expire_time` = ? where `token` = ?",
+          [expire_time, req.headers.authorization]
         )
+      }
+      return response(200, {
+        "login": !!result,
+        "userid": result?.userid || null,
+        "nickname": result?.nickname || null
       })
-  }
+    })
 }
 
 module.exports = userdata
