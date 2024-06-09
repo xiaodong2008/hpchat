be0089ad6e3e2ec38c0b2b820070db2609109ec1
xiaodong2008 <admin@xiaodong.moe>
2024-06-10 02:13:25 +0800

    feat: init primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from "vue";
+import "primevue/resources/themes/aura-light-green/theme.css";
 import "./style.css";
-import App from "./App.vue";
 
-createApp(App).mount("#app");
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
+
+const app = createApp(App);
+app.use(PrimeVue);
+
+app.mount("#app");
